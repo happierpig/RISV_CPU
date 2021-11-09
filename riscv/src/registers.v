ff8a6c839942187ca7fd99f5848159cb9985c251
happierpig <zhaoyilong217@sjtu.edu.cn>
2021-11-09 14:05:45 +0800

    [Debug] add ce_signal from fetcher to denote whether destination's busy tag can be modified.

--- riscv/src/registers.v
+++ riscv/src/registers.v
@@ -1,7 +1,10 @@
 `include "/Users/dreamer/Desktop/Programm/大二 上/计算机系统/CPU/riscv/src/constant.v"
-
+`define debug
 module registers (
     input clk,input rst,input rdy,
+
+    // from fetcher to decide whether to update the destination's busy tag 
+    input in_fetcher_ce,
 
     // Decoder asks for Value/ROB-Tag/Busy of two operands. ps: read-only
     input [`REG_TAG_WIDTH] in_decode_reg_tag1,
@@ -59,11 +62,20 @@ module registers (
                 if(rst == `FALSE && rdy == `TRUE) begin
                     if(in_rob_commit_reg == k) begin
                         values[k] <= in_rob_commit_value;
+                        `ifdef debug
+                            $display($time,"Reg ",k," 's value is set to ",in_rob_commit_value," by ",in_rob_commit_rob);
+                        `endif
                         if(in_rob_commit_rob == tags[k]) begin
+                            `ifdef debug
+                                $display($time,"Reg ",k," 's busy tag is false by ",in_rob_commit_rob);
+                            `endif
                             busy[k] <= `FALSE;
                         end
                     end
-                    if(in_decode_destination_reg == k) begin
+                    if(in_fetcher_ce == `TRUE && in_decode_destination_reg == k) begin
+                        `ifdef debug
+                            $display($time,"Reg ",k," 's busy tag is true,and destination is ",in_decode_destination_rob);
+                        `endif
                         busy[k] <= `TRUE;
                         tags[k] <= in_decode_destination_rob;
                     end
