db68dc0f54906b46684d46883a7d8497947da407
happierpig <zhaoyilong217@sjtu.edu.cn>
2021-11-14 20:39:14 +0800

    Add i-cache

--- riscv/src/fetcher.v
+++ riscv/src/fetcher.v
@@ -33,6 +33,12 @@ module fetcher (
     reg [`DATA_WIDTH] pc;
     wire next_idle = in_rs_idle && in_lsb_idle && in_rob_idle;
 
+    // 0.5KB i-cache  Size can be modifier by changing parameter
+    reg [24:0] icache_tag [(`ICACHE_SIZE-1):0];
+    reg [`DATA_WIDTH] icache_instr [(`ICACHE_SIZE-1):0];
+    reg icache_valid [(`ICACHE_SIZE-1):0];
+
+    integer i;
     always@(posedge clk) begin
         if(rst == `TRUE) begin 
             status <= IDLE;
@@ -42,6 +48,9 @@ module fetcher (
             out_instr <= `ZERO_DATA;
             out_pc <= `ZERO_DATA;
             out_store_ce <= `FALSE;
+            for(i=0;i < `ICACHE_SIZE;i=i+1) begin
+                icache_valid[i] <= `FALSE;
+            end 
         end else if(rdy == `TRUE) begin 
             out_mem_ce <= `FALSE;
             out_store_ce <= `FALSE;
@@ -50,25 +59,36 @@ module fetcher (
                 pc <= in_rob_newpc;
             end else begin
                 if(status == IDLE) begin
-                    status <= WAIT_MEM;
-                    out_mem_ce <= `TRUE;
-                    out_mem_pc <= pc;
+                    // cache hit
+                    if(icache_valid[pc[`ICACHE_INDEX_WIDTH]] == `TRUE && icache_tag[pc[`ICACHE_INDEX_WIDTH]] == pc[`ICACHE_TAG_WIDTH]) begin 
+                        out_instr <= icache_instr[pc[`ICACHE_INDEX_WIDTH]];
+                        out_pc <= pc;
+                        status <= WAIT_IDLE;
+                    end else begin  // cache miss
+                        status <= WAIT_MEM;
+                        out_mem_ce <= `TRUE;
+                        out_mem_pc <= pc;
+                    end
                 end else if(status == WAIT_MEM) begin 
                     if(in_mem_ce == `TRUE) begin 
                         out_instr <= in_mem_instr;
                         out_pc <= pc;
-                        if(next_idle == `TRUE) begin 
-                            out_store_ce <= `TRUE;
-                            status <= IDLE;
-                        end else begin status <= WAIT_IDLE; end
+                        status <= WAIT_IDLE;
+                        // change i-cache entry
+                        icache_valid[pc[`ICACHE_INDEX_WIDTH]] <= `TRUE;
+                        icache_tag[pc[`ICACHE_INDEX_WIDTH]] <= pc[`ICACHE_TAG_WIDTH];
+                        icache_instr[pc[`ICACHE_INDEX_WIDTH]] <= in_mem_instr;
                         // branch instruction
-                        if(in_mem_instr[`OPCODE_WIDTH] == 7'b1101111) begin
-                            pc <= pc + {{12{in_mem_instr[31]}}, in_mem_instr[19:12], in_mem_instr[20], in_mem_instr[30:25], in_mem_instr[24:21], 1'b0};
-                        end else begin pc <= pc + 4; end
                     end
                 end else if(status == WAIT_IDLE && next_idle == `TRUE) begin 
                     out_store_ce <= `TRUE;
                     status <= IDLE;
+                    if(out_instr[`OPCODE_WIDTH] == 7'b1101111) begin
+                        pc <= pc + {{12{out_instr[31]}}, out_instr[19:12], out_instr[20], out_instr[30:25], out_instr[24:21], 1'b0};
+                    end else begin pc <= pc + 4; end
+                    `ifdef debug 
+                        $display($time," [Fetcher] Output PC : %h",pc);
+                    `endif
                 end
             end
         end
